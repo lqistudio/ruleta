a31b201d88b312369bb804bc148154981301cc0a
lqistudio <loquiestudio@gmail.com>
2025-09-09 02:16:35 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('Cliente conectado');
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Servidor escuchando en http://localhost:3000');
+});
